9e371033bc368385acfcd3834cb0beb06c9498c2
JorheayUAEM <jayalah001@alumno.uaemex.mx>
2022-08-17 23:57:53 -0500

    Test commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from automata import cAutomata
 
 def main():
-    automata = cAutomata("C:/Users/Jorheay/Documents/UAEM/Proyects/ReconocimientoDePatrones/texto.txt")
+    automata = cAutomata("texto.txt")
     #automata.reconoceNV()
     #automata.comment()
     automata.reconoceComent()
